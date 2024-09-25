a16257e02946d1fc0bff2dd88a594735f4d79cd9
ThinMaw <thinthinmaw811@gmail.com>
2024-09-25 21:33:22 +0630

    update forecast js scirpt

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ function refreshWeather(response) {
   forcastElement.innerHTML = response.data.condition.description;
   iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-app-icon" />`;
   timeElement.innerHTML = formatDate(date);
-  console.log(response);
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -50,6 +51,48 @@ function shCity(event) {
   event.preventDefault();
   let searchCity = document.querySelector("#enter-search-place");
   cityUrl(searchCity.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(city) {
+  let apiKey = "61f4f92abtf2d414oe889522f6b3804a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
 }
 
 let searchElement = document.querySelector("#search-form");
